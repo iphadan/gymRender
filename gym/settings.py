ef4c2a8587721531df8ca168643e667b1e022494
iphadan <ephadaniel177@gmail.com>
2024-05-07 10:47:27 +0300

    / removed to media root

--- gym/settings.py
+++ gym/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'/media/')
+MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
 
 
 STATIC_URL = '/static/'
