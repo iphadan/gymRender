684d27dfa986f978f71ab77e382d5024bb61ec83
iphadan <ephadaniel177@gmail.com>
2024-05-31 13:32:15 +0300

    logo2.png removed

--- gym/settings.py
+++ gym/settings.py
@@ -18,14 +18,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 
-SECRET_KEY = 'django-insecure-t85ojesbszrra5=1m98sex5kx%4avpzpw#^q_^57hk0%e0itf%'
-#SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = 'django-insecure-t85ojesbszrra5=1m98sex5kx%4avpzpw#^q_^57hk0%e0itf%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = os.environ.get('DEBUG','False').lower() == 'true'
-DEBUG = False
+DEBUG = os.environ.get('DEBUG','False').lower() == 'true'
+#DEBUG = False
 ALLOWED_HOSTS = ['gymrender.onrender.com','localhost','127.0.0.1']
 
 
@@ -90,8 +90,8 @@ DATABASES = {
 }
 
 
-# database_url=os.environ.get('DATABASE_URL')
-# DATABASES['default'] = dj_database_url.parse(database_url)
+database_url=os.environ.get('DATABASE_URL')
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 LOGGING = {
     'version': 1,
